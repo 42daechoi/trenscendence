ab701db53dd267455a2b4c53ea272a83c6f349e8
yugd1211 <yugd1211@gmail.com>
2023-07-27 18:22:04 +0900

    WIP: profile overlay setting...

--- my-app/src/component/FriendsList.tsx
+++ my-app/src/component/FriendsList.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../css/Modal.css";
 
-export default function friends_list() {
+const Modal = ({ closeModal }) => {
+  const stopPropagation = (e: any) => {
+    e.stopPropagation();
+  };
+  return (
+    // <div className="profile-modal-back" onClick={closeModal}>
+    <>
+      <div
+        className="modal-overlay"
+        style={{ position: "fixed", width: "100vw", height: "100vh" }}
+        onClick={closeModal}
+      ></div>
+      <div className="profile-modal" onClick={stopPropagation}>
+        <button className="close" onClick={closeModal}>
+          Close
+        </button>
+        <h2>This is a modal</h2>
+        <p>Some content...</p>
+      </div>
+      {/* // </div> */}
+    </>
+  );
+};
+
+export default function Friends_list() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div>
-      <li><a className='chat_btn'><div>🟢</div><div>daechoi</div></a></li>
-      <li><a className='chat_btn'><div>🔴</div><div>endisnl</div></a></li>
-      <li><a className='chat_btn'><div>🔴</div><div>endisnl</div></a></li>
+      <li>
+        <a className="chat_btn" onClick={openModal}>
+          <div>🟢</div>
+          <div>daechoi</div>
+        </a>
+      </li>
+      <li>
+        <a className="chat_btn" onClick={openModal}>
+          <div>🔴</div>
+          <div>endisnl</div>
+        </a>
+      </li>
+      <li>
+        <a className="chat_btn" onClick={openModal}>
+          <div>🔴</div>
+          <div>endisnl</div>
+        </a>
+      </li>
+      {isModalOpen && <Modal closeModal={closeModal} />}
     </div>
-  )
+  );
 }
