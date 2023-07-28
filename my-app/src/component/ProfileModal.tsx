ec5d8e07e68fdaa91ceb660abb0b95716f383747
yugd1211 <yugd1211@gmail.com>
2023-07-28 12:37:56 +0900

    FIX: add modal window background color

--- my-app/src/component/ProfileModal.tsx
+++ my-app/src/component/ProfileModal.tsx
@@ -20,6 +20,8 @@ const Modal = ({ closeModal }) => {
           right: "0%",
           top: "0%",
           zIndex: "999",
+          // 배경을 투명하게 한다. 이미 모달을 연상태에서는 배경이 더진해진다
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
         }}
         onClick={closeModal}
       ></div>
