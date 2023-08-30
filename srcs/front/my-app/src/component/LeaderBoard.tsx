05618b0c1e4cf153729885c6b857b1c4d7d8a2b3
yugd1211 <yugd1211@gmail.com>
2023-08-30 18:03:07 +0900

    REFACT: Delete Annotation Code

--- srcs/front/my-app/src/component/LeaderBoard.tsx
+++ srcs/front/my-app/src/component/LeaderBoard.tsx
@@ -30,30 +30,6 @@ export default function LeaderBoard() {
             <div className="ratio">{data.rate}</div>
           </li>
         ))}
-        {/* <li className="ranklist">
-          <div className="rank" style={{ color: "gold" }}>
-            1
-          </div>
-          <div className="nickname">daechoi</div>
-          <div className="number">30전 20승 10패</div>
-          <div className="ratio">66.6%</div>
-        </li>
-        <li className="ranklist">
-          <div className="rank" style={{ color: "silver" }}>
-            2
-          </div>
-          <div className="nickname">김두한</div>
-          <div className="number">30전 10승 20패</div>
-          <div className="ratio">33.3%</div>
-        </li>
-        <li className="ranklist">
-          <div className="rank" style={{ color: "#af6114" }}>
-            3
-          </div>
-          <div className="nickname">김두한</div>
-          <div className="number">10전 10승 0패</div>
-          <div className="ratio">100%</div>
-        </li> */}
       </ul>
     </div>
   );
