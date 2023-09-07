fad9312b22be498f83938f29459e4fc176a4635a
mac <daechoi@macui-MacBookAir-2.local>
2023-09-07 17:34:43 +0900

    fix

--- srcs/front/my-app/src/page/Notfound.tsx
+++ srcs/front/my-app/src/page/Notfound.tsx
@@ -4,9 +4,9 @@ import '../css/404.css';
 const NotFound: React.FC = () => {
   return (
     <div className="background">
-      <h1>404</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-      <a href="/">로그인 페이지로 돌아가기</a>
+      <h1 className='nfh'>404</h1>
+      <p className='nfp'>페이지를 찾을 수 없습니다.</p>
+      <a className='nfa' href="/">로그인 페이지로 돌아가기</a>
     </div>
   );
 }
